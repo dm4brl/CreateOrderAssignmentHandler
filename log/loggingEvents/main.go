0ba1b9b9181f02161fe8a932bc26940dd468c86e
dm4brl <92757328+dm4brl@users.noreply.github.com>
2023-10-25 16:02:32 -0300

    Update main.go

--- log/loggingEvents/main.go
+++ log/loggingEvents/main.go
@@ -21,3 +21,18 @@ func writeLogToFile(logEvent LogEvent, logFile *os.File) error {
     _, err = logFile.WriteString(string(logEntry) + "\n")
     return err
 }
+func analyzeLogs(logFile *os.File) ([]LogEvent, error) {
+    var logEvents []LogEvent
+
+    scanner := bufio.NewScanner(logFile)
+    for scanner.Scan() {
+        logEntry := scanner.Text()
+        var logEvent LogEvent
+        if err := json.Unmarshal([]byte(logEntry), &logEvent); err != nil {
+            return nil, err
+        }
+        logEvents = append(logEvents, logEvent)
+    }
+
+    return logEvents, scanner.Err()
+}
