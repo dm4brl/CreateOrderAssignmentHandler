1dd56ed46d86de68c84864fe504b12c81af02a3b
dm4brl <92757328+dm4brl@users.noreply.github.com>
2023-10-25 16:01:11 -0300

    Update main.go

--- log/loggingEvents/main.go
+++ log/loggingEvents/main.go
@@ -3,3 +3,10 @@ type LogEvent struct {
     Message  string
     Analysis string
 }
+logEvent := LogEvent{
+    Time:    time.Now(),
+    Message: "Non-standard event detected",
+    Analysis: "This event does not conform to the expected pattern. Additional analysis may be needed.",
+}
+log.Printf("Event: %s", logEvent.Message)
+// Log the event to a file or other storage
